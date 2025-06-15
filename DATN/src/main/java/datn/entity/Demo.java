6adc8d83b5d6966f84ae832ab42dffeb491cc2b2
phuccc05 <phucnhph50219@gmail.com>
2025-06-15 23:49:02 +0700

    sửa2

--- DATN/src/main/java/datn/entity/Demo.java
+++ DATN/src/main/java/datn/entity/Demo.java
@@ -1,7 +1,19 @@
 package datn.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Demo {
     private String name;
 
     private String ten;
+
+    private String name1;
+
+    private String ten1;
+    private String name2;
+
+    private String ten2;
 }
